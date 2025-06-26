30779b08d95b44fe114e9b2b21f142b0a0d17c2b
nahashon <nashonmwendwa0@gmail.com>
2025-06-26 09:25:31 +0300

    downgraded the tailwind and installed a lower version that is working

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Home from './pages/Home/Home';
 import Home1 from './pages/Home/Home1';
 import Home2 from './pages/Home/Home2';
 import Home3 from './pages/Home/Home3';
+import './output.css';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home1" element={<Home1 />} />
-        <Route path="/home2" element={<Home2 />} />
-        <Route path="/home3" element={<Home3 />} />
-      </Routes>
+      <div className="bg-gray-900 min-h-screen text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home1" element={<Home1 />} />
+          <Route path="/home2" element={<Home2 />} />
+          <Route path="/home3" element={<Home3 />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
